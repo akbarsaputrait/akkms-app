1b32f88a1fe62d0395b72c7a74860a23bce05a4a
akbarsaputrait <akbarsaputrait@outlook.com>
2022-11-23 18:37:49 +0700

    Fix lint

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,3 @@
-
-
 // seedClassData();
 
 import { seedStudent } from "./seeder/student";
--- src/pages/api/students/index.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from "next";
-
-export default async (req: NextApiRequest, res: NextApiResponse) => {
-  switch (req.method) {
-    case "GET": {
-      res.json({ status: "Ok" });
-    }
-  }
-};
