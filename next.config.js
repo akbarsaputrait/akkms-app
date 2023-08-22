16e869d2371ae3149e0cad91a219a6e7b56b3fa5
akbarsaputrait <akbarsaputrait@outlook.com>
2023-08-22 14:28:35 +0700

    Analytics

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 module.exports = {
   env: {
     API_URL: process.env.API_URL,
+    DATABASE_URL: process.env.DATABASE_URL,
+    MODE: process.env.MODE,
   },
 };
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,15 +1,19 @@
 import "../styles/globals.css";
 
 import { ThemeProvider } from "@material-tailwind/react";
+import { Analytics } from "@vercel/analytics/react";
 import { SWRConfig } from "swr/_internal";
 
 import { fetcher } from "../utils";
 
 function MyApp({ Component, pageProps }) {
+  const mode: any = process.env.MODE;
+
   return (
     <SWRConfig value={{ fetcher }}>
       <ThemeProvider>
         <Component {...pageProps} />
+        <Analytics mode={mode} />
       </ThemeProvider>
     </SWRConfig>
   );
